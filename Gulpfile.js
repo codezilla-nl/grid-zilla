984e1ecbe3cda747c9cbf8c34f63f55da60cf53e
Huib van Geertruy <hvgeertruy@gmail.com>
2015-11-20 15:03:32 +0100

    removed defer (q)

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,15 +2,10 @@ var child_process = require('child_process'),
     del = require('del'),
     gulp = require('gulp'),
     sass = require('gulp-sass');
-    autoprefixer = require('gulp-autoprefixer'),
-    q = require('q');
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('clean-css', function() {
-    var deferred = q.defer();
-    del(['style/css/**/*'], function() {
-        deferred.resolve();
-    });
-    return deferred.promise;
+    del(['style/css/**/*']);
 });
 
 gulp.task('build', ['clean-css'], function() {
